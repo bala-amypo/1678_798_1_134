1bd49fcb7ea1a50c78c0339a802508a33355a887
bala-amypo <bala@amypo.in>
2025-12-18 09:11:10 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -10,8 +10,10 @@ import java.io.IOException;
 public class SimpleStatusServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setStatus(200);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+
+        resp.setStatus(HttpServletResponse.SC_OK);
         resp.getWriter().write("Post-Surgery Recovery Tracker is running");
     }
 }
