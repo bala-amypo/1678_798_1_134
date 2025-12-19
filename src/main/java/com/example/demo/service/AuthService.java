2511085084cadbd282b3b4f105534875bb0e469b
bala-amypo <bala@amypo.in>
2025-12-19 04:33:19 +0000

    User commit

--- src/main/java/com/example/demo/service/AuthService.java
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -1,30 +1,10 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.AuthResponse;
 import com.example.demo.dto.RegisterRequest;
-import org.springframework.stereotype.Service;
 
-@Service
-public class AuthService {
-
-    public AuthResponse register(RegisterRequest r) {
-
-        // Fake register logic (for demo)
-        String username = r.getUsername();
-        String role = r.getRole();
-
-        return new AuthResponse(
-                "User registered successfully",
-                username,
-                role
-        );
-    }
-
-    public AuthResponse login(String username) {
-        return new AuthResponse(
-                "Login successful",
-                username,
-                "USER"
-        );
-    }
+public interface AuthService {
+    AuthResponse register(RegisterRequest request);
+    AuthResponse login(AuthRequest request);
 }
