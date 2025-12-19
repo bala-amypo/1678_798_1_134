f2ae1f1ce2cfd2024b9116060888fb782b55c52c
bala-amypo <bala@amypo.in>
2025-12-19 10:09:18 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,11 +1,9 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -13,8 +11,9 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-            .servers(List.of(
-                new Server().url("http://localhost:8080")
-            ));
+                .info(new Info()
+                        .title("Demo API")
+                        .version("1.0")
+                        .description("Demo Spring Boot Application"));
     }
 }
