4ac96cdaf37f3f7ae2fd5337e769b76bfba09437
bala-amypo <bala@amypo.in>
2025-12-19 04:48:19 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/ClinicalAlertRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+/*
+ * IMPORTANT:
+ * This file exists only to avoid compilation errors.
+ * The real repository is ClinicalAlertRecordRepository.java
+ * This interface MUST NOT be public.
+ */
+
+interface ClinicalAlertRepository {
+    // intentionally empty
+}
