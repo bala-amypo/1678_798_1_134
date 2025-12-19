c1b8c26c75bf803b09c6ebe03430631f94a7d001
bala-amypo <bala@amypo.in>
2025-12-19 09:15:20 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,9 +1,11 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
@@ -12,9 +14,13 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
+                .info(new Info()
+                        .title("Recovery Tracker API")
+                        .description("Post-surgery recovery monitoring system")
+                        .version("1.0.0")
+                )
                 .servers(List.of(
-                        new Server().url("https://9144.408procr.amypo.ai/")
+                        new Server().url("http://localhost:8080")
                 ));
-        }
+    }
 }
