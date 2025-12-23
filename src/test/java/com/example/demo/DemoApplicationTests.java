0f57043e42c9dfd5e3bb5bf67403da292d9b6236
bala-amypo <bala@amypo.in>
2025-12-23 14:27:34 +0000

    User commit

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,12 +1,11 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
 
-@SpringBootApplication
-public class DemoApplication {
+class DemoApplicationTests {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+    @Test
+    void applicationStarts() {
+        assert true;
     }
 }
