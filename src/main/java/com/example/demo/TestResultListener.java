f7e6ccd45dca3c263c44280373d218bf332fdf0f
bala-amypo <bala@amypo.in>
2025-12-24 06:41:32 +0000

    User commit

--- src/main/java/com/example/demo/TestResultListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-/**
- * Dummy listener to satisfy @Listeners annotation in tests.
- * No TestNG dependency required.
- */
-public class TestResultListener {
-    // intentionally empty
-}
