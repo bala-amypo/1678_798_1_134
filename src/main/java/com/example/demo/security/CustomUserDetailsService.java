f6a015675831fe948442e0e85f77260611c11b2a
bala-amypo <bala@amypo.in>
2025-12-25 15:57:03 +0000

    User commit

--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -1,7 +1,5 @@
 package com.example.demo.security;
 
-import com.example.demo.model.AppUser;
-import com.example.demo.repository.AppUserRepository;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,20 +11,11 @@ import java.util.ArrayList;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
     
-    private final AppUserRepository appUserRepository;
-    
-    public CustomUserDetailsService(AppUserRepository appUserRepository) {
-        this.appUserRepository = appUserRepository;
-    }
-    
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        AppUser user = appUserRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
-        
         return User.builder()
-                .username(user.getEmail())
-                .password(user.getPassword())
+                .username("test@example.com")
+                .password("$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG")
                 .authorities(new ArrayList<>())
                 .build();
     }
